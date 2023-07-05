970ac33f53c3e3efe1376a926d06108d739a02f7
Nurjahon Ergashev <lobarqochqorova18@gmail.com>
2023-07-06 00:47:47 +0500

    uslugi

--- src/components/Uslugi/Card/index.tsx
+++ src/components/Uslugi/Card/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Button from "../../Button";
 import styles from "./index.module.scss";
 export function UslugiCard(props: {
   img: any;
